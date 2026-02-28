3f919b17e4fcd5e275d4205f7f0dba95e55c7e78
Nobutaka Kito <62008130+nkito@users.noreply.github.com>
2026-02-28 23:38:24 +0900

    Containers for H8/300H were added

---
+++ H8_300H/gcc10/docker_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Git リポジトリのトップディレクトリを取得
+TOPLEVEL=$(git rev-parse --show-toplevel 2>/dev/null)
+
+if [ $? -eq 0 ]; then
+    echo "リポジトリのトップディレクトリ: $TOPLEVEL"
+else
+    echo "エラー: カレントディレクトリは Git リポジトリ内にありません"
+    exit 1
+fi
+
+uid=$(id -u)
+gid=$(id -g)
+
+docker run -it --rm -v $TOPLEVEL:/src -u $uid:$uid h8sdk_gcc10
---
+++ H8_300H/gcc13/docker_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t h8sdk_gcc13 .
